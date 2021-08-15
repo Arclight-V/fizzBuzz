2cdcc1a0a38a3a487ceff5e3211a64b1bb34ab1a
anatashi <anatashi@student.21-school.ru>
2021-08-15 10:41:17 +0300

    create a producer

---
+++ FizzBuzz/src/main/java/controller/NumbersController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("numbers")
+public class NumbersController {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping
+    public void sendOrder(String msgId, String msg){
+        kafkaTemplate.send("msg", msgId, msg);
+    }
+}
