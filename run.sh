144af543036987d11088e36e9251036f8a229053
anatashi <anatashi@student.21-school.ru>
2021-08-16 18:41:48 +0300

    fied bugs

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cd Kafka && docker-compose up -d && cd ..;
-docker-compose exec kafka kafka-topics.sh --create --bootstrap-server localhost:9092 --topic numbers;
-bash numbersGenerate.sh && cat ~/Desktop/fizzBuzz/numbers.txt | docker-compose exec -T kafka kafka-console-producer.sh --topic numbers --bootstrap-server localhost:9092;
+cd Kafka && docker-compose up -d && cd .. && cd Kafka;
+docker-compose exec kafka kafka-topics.sh --create --bootstrap-server localhost:9092 --topic numbers; cd ..;
+bash numbersGenerate.sh && cd Kafka && cat ~/Desktop/fizzBuzz/numbers.txt | docker-compose exec -T kafka kafka-console-producer.sh --topic numbers --bootstrap-server localhost:9092 && cd ..;
 mvn package /Kafka && mvn exec:java /Kafka
