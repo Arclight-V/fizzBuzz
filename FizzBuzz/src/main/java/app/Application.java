3fe5f3847e45f2dea74b3c6d22a672437abda586
anatashi <anatashi@student.21-school.ru>
2021-08-16 10:26:49 +0300

    add main

--- FizzBuzz/src/main/java/app/Application.java
+++ FizzBuzz/src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import algorithm.Algorithm;
 import algorithm.FizzBuzzAlgorithm;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,22 +11,13 @@ import org.springframework.kafka.annotation.KafkaListener;
 @SpringBootApplication
 public class Application {
 
-    private enum action {PRINT, FILE};
-    action action;
-
-    Application(String str) {
-        if (str.equals("print")) {
-            action = action.PRINT;
-        } else {
-            action = action.FILE;
-        }
-    }
-
     @KafkaListener(topics = "numbers")
     public void NumbersListener(String numbers) {
         Algorithm algorithm = new FizzBuzzAlgorithm(numbers);
-
+        algorithm.printAll();
     }
 
-    public static void main()
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
